20bfb3279a78058d2bb2f65c94dbef00de40a997
Luke Pothier <lukepothier@gmail.com>
2017-06-10 12:22:52 +0200

    Added and implemented IDisposable

--- src/EZRandom.cs
+++ src/EZRandom.cs
@@ -1,6 +1,33 @@
-﻿namespace EZRNG
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace EZRNG
 {
-    public class EZRandom
+    public class EZRandom : IDisposable
     {
+        bool _disposed;
+        SafeHandle _handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        #region GC
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                _handle.Dispose();
+
+            _disposed = true;
+        }
+
+        #endregion GC
     }
 }
