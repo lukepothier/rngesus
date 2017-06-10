a46087bb0651cf68fe508b8bfa3d6ee4f243024a
Luke Pothier <lukepothier@gmail.com>
2017-06-10 21:51:40 +0200

    Added RNG for UInt32

--- src/EZRandom.cs
+++ src/EZRandom.cs
@@ -1,33 +1,73 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using Microsoft.Win32.SafeHandles;
+using System.Security.Cryptography;
 
 namespace EZRNG
 {
-    public class EZRandom : IDisposable
+    public class EZRandom
     {
-        bool _disposed;
-        SafeHandle _handle = new SafeFileHandle(IntPtr.Zero, true);
+        #region Init/Ctor
 
-        #region GC
+        static RNGCryptoServiceProvider _prng;
 
-        public void Dispose()
+        static EZRandom()
         {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            _prng = new RNGCryptoServiceProvider();
         }
 
-        protected virtual void Dispose(bool disposing)
+        #endregion Init/Ctor
+
+        #region UInt32
+
+        /// <summary>
+        /// Creates a cryptographically secure unsigned 32-bit integer
+        /// </summary>
+        /// <returns>A random, unsigned 32-bit integer</returns>
+        public static uint GenerateInteger()
         {
-            if (_disposed)
-                return;
+            var buffer = new byte[sizeof(uint)];
+            _prng.GetBytes(buffer);
 
-            if (disposing)
-                _handle.Dispose();
-
-            _disposed = true;
+            return BitConverter.ToUInt32(buffer, 0);
         }
 
-        #endregion GC
+        /// <summary>
+        /// Creates a cryptographically secure unsigned 32-bit integer equal to or below a provided maximum
+        /// </summary>
+        /// <param name="maximum">The maximum value of the integer required</param>
+        /// <returns>A random unsigned 32-bit integer equal to or below the provided maximum</returns>
+        public static uint GenerateInteger(uint maximum)
+            => GenerateInteger(uint.MinValue, maximum);
+
+        /// <summary>
+        /// Creates a cryptographically secure unsigned 32-bit integer within a provided range
+        /// </summary>
+        /// <param name="minimum">The minimum value of the integer required</param>
+        /// <param name="maximum">The maximum value of the integer required</param>
+        /// <returns>A random unsigned 32-bit integer within the provided range</returns>
+        public static uint GenerateInteger(uint minimum, uint maximum)
+        {
+            if (minimum > maximum)
+                throw new ArgumentOutOfRangeException(nameof(minimum), "minimum cannot exceed maximum");
+
+            if (minimum == maximum)
+                return minimum;
+
+            ulong difference = maximum - minimum;
+            while (true)
+            {
+                var buffer = new byte[sizeof(uint)];
+                _prng.GetBytes(buffer);
+                var randomInteger = BitConverter.ToUInt32(buffer, 0);
+
+                const ulong max = ((ulong)uint.MaxValue + 1);
+                var remainder = max % difference;
+                if (randomInteger < max - remainder)
+                {
+                    return (uint)(minimum + (randomInteger % difference));
+                }
+            }
+        }
+
+        #endregion UInt32
     }
 }
