42fe1abf1f72c2770a4bf4928d4d6f6f6f0da01b
Luke Pothier <lukepothier@gmail.com>
2017-06-10 22:04:51 +0200

    Added RNG for UInt64

--- src/EZRandom.cs
+++ src/EZRandom.cs
@@ -53,6 +53,7 @@ namespace EZRNG
                 return minimum;
 
             ulong difference = maximum - minimum;
+
             while (true)
             {
                 var buffer = new byte[sizeof(uint)];
@@ -69,5 +70,60 @@ namespace EZRNG
         }
 
         #endregion UInt32
+
+        #region UInt64
+
+        /// <summary>
+        /// Creates a cryptographically secure unsigned 64-bit integer
+        /// </summary>
+        /// <returns>A random, unsigned 64-bit integer</returns>
+        public static ulong GenerateLong()
+        {
+            var buffer = new byte[sizeof(ulong)];
+            _prng.GetBytes(buffer);
+
+            return BitConverter.ToUInt64(buffer, 0);
+        }
+
+        /// <summary>
+        /// Creates a cryptographically secure unsigned 64-bit integer equal to or below a provided maximum
+        /// </summary>
+        /// <param name="maximum">The maximum value of the integer required</param>
+        /// <returns>A random unsigned 64-bit integer equal to or below the provided maximum</returns>
+        public static ulong GenerateLong(ulong maximum)
+            => GenerateLong(ulong.MinValue, maximum);
+
+        /// <summary>
+        /// Creates a cryptographically secure unsigned 64-bit integer within a provided range
+        /// </summary>
+        /// <param name="minimum">The minimum value of the integer required</param>
+        /// <param name="maximum">The maximum value of the integer required</param>
+        /// <returns>A random unsigned 64-bit integer within the provided range</returns>
+        public static ulong GenerateLong(ulong minimum, ulong maximum)
+        {
+            if (minimum > maximum)
+                throw new ArgumentOutOfRangeException(nameof(minimum), "minimum cannot exceed maximum");
+
+            if (minimum == maximum)
+                return minimum;
+
+            var difference = maximum - minimum;
+
+            while (true)
+            {
+                var buffer = new byte[sizeof(ulong)];
+                _prng.GetBytes(buffer);
+                var randomInteger = BitConverter.ToUInt64(buffer, 0);
+
+                const ulong max = (ulong.MaxValue);
+                var remainder = max % difference;
+                if (randomInteger < max - remainder)
+                {
+                    return (minimum + (randomInteger % difference));
+                }
+            }
+        }
+
+        #endregion UInt64
     }
 }
