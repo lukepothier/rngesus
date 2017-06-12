03f619ce9ec8bd4c68fc5a9102c14b859d2f61e4
Luke Pothier <lukepothier@gmail.com>
2017-06-12 19:21:42 +0200

    Changed from static to instance type, added thread safety, changed return types for ease-of-use

--- src/RNGesus/RNGesus.cs
+++ src/RNGesus/RNGesus.cs
@@ -1,27 +1,71 @@
 ﻿// RNGesus.cs
+// Copyright(c) 2017 Luke Pothier
 // RNGesus is licensed using the MIT License
 // Author: Luke Pothier <lukepothier@gmail.com>
 
 using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.Win32.SafeHandles;
 
-namespace RNGesus
+namespace Luke.RNG
 {
     /// <summary>
-    /// Static secure pseudorandom generator
+    /// Secure pseudorandom value generator
     /// </summary>
-    public static class RNGesus
+    public class RNGesus : IDisposable
     {
         #region Init/Ctor
 
-        static readonly RNGCryptoServiceProvider _prng;
+        bool _disposed;
+        readonly SafeHandle _handle = new SafeFileHandle(IntPtr.Zero, true);
+        const int DEFAULT_SHARED_BUFFER_LENGTH = 1024;
+        static RNGCryptoServiceProvider _prng;
+        byte[] _sharedBuffer;
+        int _currentBufferIndex;
+        readonly int _bufferLength;
 
-        static RNGesus()
+        /// <summary>
+        /// Secure pseudorandom value generator.
+        /// The parameterless constructor initialises RNGesus with up to 1024 byte operations supported. If you require outputs larger than 1024 bytes, consider using the overload.
+        /// </summary>
+        public RNGesus()
+            : this(DEFAULT_SHARED_BUFFER_LENGTH)
+        {
+        }
+
+        /// <summary>
+        /// Secure pseudorandom value generator.
+        /// <param name="sharedBufferSize">The maximum size of outputs you expect to need to generate. Larger buffer size will result in higher memory use.</param>
+        /// </summary>
+        public RNGesus(int sharedBufferSize)
         {
             _prng = new RNGCryptoServiceProvider();
+            _bufferLength = sharedBufferSize;
+        }
+
+        void InitSharedBuffer()
+        {
+            if (_sharedBuffer == null || _sharedBuffer.Length != _bufferLength)
+                _sharedBuffer = new byte[_bufferLength];
+
+            _prng.GetBytes(_sharedBuffer);
+            _currentBufferIndex = 0;
+        }
+
+        void RequestBuffer(int bytesRequired)
+        {
+            if (_sharedBuffer == null)
+                InitSharedBuffer();
+
+            if (bytesRequired > _bufferLength)
+                throw new ArgumentOutOfRangeException(nameof(bytesRequired), "Number of bytes required exceeds the size of the shared buffer. Re-initialise RNGesus with a larger buffer size.");
+
+            if ((_sharedBuffer.Length - _currentBufferIndex) < bytesRequired)
+                InitSharedBuffer();
         }
 
         #endregion Init/Ctor
@@ -34,12 +78,22 @@ namespace RNGesus
         /// <returns>
         /// A random unsigned 32-bit random integer
         /// </returns>
-        public static uint GenerateInt()
+        public int GenerateInt()
         {
-            var buffer = new byte[sizeof(uint)];
-            _prng.GetBytes(buffer);
+            lock (this)
+            {
+                RequestBuffer(sizeof(uint));
 
-            return BitConverter.ToUInt32(buffer, 0);
+                _prng.GetBytes(_sharedBuffer);
+
+                var result = BitConverter.ToUInt32(_sharedBuffer, _currentBufferIndex);
+
+                _currentBufferIndex += sizeof(uint);
+
+                return (int)result >= 0
+                    ? (int)result
+                    : (int)result * -1;
+            }
         }
 
         /// <summary>
@@ -49,7 +103,7 @@ namespace RNGesus
         /// <returns>
         /// A random unsigned 32-bit random integer equal to or below the provided maximum
         /// </returns>
-        public static uint GenerateInt(uint maximum)
+        public int GenerateInt(uint maximum)
             => GenerateInt(uint.MinValue, maximum);
 
         /// <summary>
@@ -60,27 +114,37 @@ namespace RNGesus
         /// <returns>
         /// A random unsigned 32-bit random integer within the provided range
         /// </returns>
-        public static uint GenerateInt(uint minimum, uint maximum)
+        public int GenerateInt(uint minimum, uint maximum)
         {
-            if (minimum > maximum)
-                throw new ArgumentOutOfRangeException(nameof(minimum), "minimum cannot exceed maximum");
-
-            if (minimum == maximum)
-                throw new ArgumentException("Only one output is possible - output is determinable from inputs");
-
-            ulong difference = maximum - minimum;
-
-            while (true)
+            lock (this)
             {
-                var buffer = new byte[sizeof(uint)];
-                _prng.GetBytes(buffer);
-                var randomInteger = BitConverter.ToUInt32(buffer, 0);
+                RequestBuffer(sizeof(uint));
 
-                const ulong max = (ulong)uint.MaxValue + 1;
-                var remainder = max % difference;
-                if (randomInteger < max - remainder)
+                if (minimum > maximum)
+                    throw new ArgumentOutOfRangeException(nameof(minimum), "minimum cannot exceed maximum");
+
+                if (minimum == maximum)
+                    throw new ArgumentException("Only one output is possible - output is determinable from inputs");
+
+                ulong difference = maximum - minimum;
+
+                while (true)
                 {
-                    return (uint)(minimum + (randomInteger % difference));
+                    _prng.GetBytes(_sharedBuffer);
+                    var randomInteger = BitConverter.ToUInt32(_sharedBuffer, _currentBufferIndex);
+
+                    const ulong max = (ulong)uint.MaxValue + 1;
+                    var remainder = max % difference;
+                    if (randomInteger < max - remainder)
+                    {
+                        var result = (uint)(minimum + (randomInteger % difference));
+
+                        _currentBufferIndex += sizeof(uint);
+
+                        return (int)result >= 0
+                            ? (int)result
+                            : (int)result * -1;
+                    }
                 }
             }
         }
@@ -95,12 +159,22 @@ namespace RNGesus
         /// <returns>
         /// A random unsigned 64-bit random integer
         /// </returns>
-        public static ulong GenerateLong()
+        public long GenerateLong()
         {
-            var buffer = new byte[sizeof(ulong)];
-            _prng.GetBytes(buffer);
+            lock (this)
+            {
+                RequestBuffer(sizeof(ulong));
 
-            return BitConverter.ToUInt64(buffer, 0);
+                _prng.GetBytes(_sharedBuffer);
+
+                var result = BitConverter.ToUInt64(_sharedBuffer, _currentBufferIndex);
+
+                _currentBufferIndex += sizeof(ulong);
+
+                return (long)result >= 0
+                    ? (long)result
+                    : (long)result * -1;
+            }
         }
 
         /// <summary>
@@ -110,7 +184,7 @@ namespace RNGesus
         /// <returns>
         /// A random unsigned 64-bit integer equal to or below the provided maximum
         /// </returns>
-        public static ulong GenerateLong(ulong maximum)
+        public long GenerateLong(ulong maximum)
             => GenerateLong(ulong.MinValue, maximum);
 
         /// <summary>
@@ -121,27 +195,36 @@ namespace RNGesus
         /// <returns>
         /// A random unsigned 64-bit integer within the provided range
         /// </returns>
-        public static ulong GenerateLong(ulong minimum, ulong maximum)
+        public long GenerateLong(ulong minimum, ulong maximum)
         {
-            if (minimum > maximum)
-                throw new ArgumentOutOfRangeException(nameof(minimum), "minimum cannot exceed maximum");
-
-            if (minimum == maximum)
-                throw new ArgumentException("Only one output is possible - output is determinable from inputs");
-
-            var difference = maximum - minimum;
-
-            while (true)
+            lock (this)
             {
-                var buffer = new byte[sizeof(ulong)];
-                _prng.GetBytes(buffer);
-                var randomInteger = BitConverter.ToUInt64(buffer, 0);
+                if (minimum > maximum)
+                    throw new ArgumentOutOfRangeException(nameof(minimum), "minimum cannot exceed maximum");
 
-                const ulong max = ulong.MaxValue;
-                var remainder = max % difference;
-                if (randomInteger < max - remainder)
+                if (minimum == maximum)
+                    throw new ArgumentException("Only one output is possible - output is determinable from inputs");
+
+                RequestBuffer(sizeof(ulong));
+
+                var difference = maximum - minimum;
+
+                while (true)
                 {
-                    return minimum + (randomInteger % difference);
+                    _prng.GetBytes(_sharedBuffer);
+                    var randomInteger = BitConverter.ToUInt64(_sharedBuffer, _currentBufferIndex);
+
+                    var remainder = ulong.MaxValue % difference;
+                    if (randomInteger < ulong.MaxValue - remainder)
+                    {
+                        var result = minimum + (randomInteger % difference);
+
+                        _currentBufferIndex += sizeof(ulong);
+
+                        return (long)result >= 0
+                            ? (long)result
+                            : (long)result * -1;
+                    }
                 }
             }
         }
@@ -156,7 +239,7 @@ namespace RNGesus
         /// <returns>
         /// A random 32-bit positive floating point number between 0 and 1
         /// </returns>
-        public static float GenerateFloat()
+        public float GenerateFloat()
             => (float)GenerateDouble();
 
         #endregion Float
@@ -169,7 +252,7 @@ namespace RNGesus
         /// <returns>
         /// A random 64-bit positive floating point number between 0 and 1
         /// </returns>
-        public static double GenerateDouble()
+        public double GenerateDouble()
             => GenerateInt() / (uint.MaxValue + 1d);
 
         #endregion Double
@@ -182,10 +265,8 @@ namespace RNGesus
         /// <returns>
         /// A random boolean
         /// </returns>
-        public static bool GenerateBool()
-        {
-            return GenerateInt() % 2 == 0;
-        }
+        public bool GenerateBool()
+            => GenerateInt() % 2 == 0;
 
         #endregion Boolean
 
@@ -198,11 +279,18 @@ namespace RNGesus
         /// <returns>
         /// An array of random bytes
         /// </returns>
-        public static byte[] GenerateByteArray(int length)
+        public byte[] GenerateByteArray(int length)
         {
-            var buffer = new byte[length];
-            _prng.GetBytes(buffer);
-            return buffer;
+            lock (this)
+            {
+                RequestBuffer(length);
+
+                var result = _sharedBuffer.Take(length).ToArray();
+
+                _currentBufferIndex += length;
+
+                return result;
+            }
         }
 
         #endregion Byte Array
@@ -216,7 +304,7 @@ namespace RNGesus
         /// <returns>
         /// A random string containing upper- and lower-case (ISO basic Latin) alphabetical letters, numbers, hyphens, and underscores of the requested length
         /// </returns>
-        public static string GenerateString(int length)
+        public string GenerateString(int length)
         {
             const string validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_";
             return GenerateString(length, validCharacters);
@@ -235,7 +323,7 @@ namespace RNGesus
         /// WARNING: If 256 cannot evenly divide the number of characters (or number of distinct characters if <paramref name="removeDuplicates"/> is true) in <paramref name="validCharacters"/>,
         /// then the entropy of the output is compromised. For best results, ensure that 256 evenly divides the number of valid characters.
         /// </remarks>
-        public static string GenerateString(int length, string validCharacters, bool removeDuplicates = true)
+        public string GenerateString(int length, string validCharacters, bool removeDuplicates = true)
         {
             var validCharacterArray = validCharacters.ToCharArray();
 
@@ -263,35 +351,63 @@ namespace RNGesus
         /// WARNING: If 256 cannot evenly divide the number of characters (or number of distinct characters if <paramref name="removeDuplicates"/> is true) in <paramref name="validCharacters"/>,
         /// then the entropy of the output is compromised. For best results, ensure that 256 evenly divides the number of valid characters.
         /// </remarks>
-        public static string GenerateString(int length, char[] validCharacters, bool removeDuplicates = true)
+        public string GenerateString(int length, char[] validCharacters, bool removeDuplicates = true)
         {
-            var distinctValidCharacterArray = validCharacters.Distinct().ToArray();
-
-            if (distinctValidCharacterArray.Length <= 1)
-                throw new ArgumentException("Only one output is possible - output is determinable from inputs", nameof(validCharacters));
-
-            if (removeDuplicates)
-                validCharacters = distinctValidCharacterArray;
-
-            var validCharactersLength = validCharacters.Length;
-
-            if (256 % validCharactersLength != 0)
-                Trace.TraceWarning("The number of valid characters supplied cannot be evenly divided by 256. The entropy of the output is compromised.");
-
-            var stringBuilder = new StringBuilder();
-
-            var buffer = new byte[sizeof(uint)];
-
-            while (length-- > 0)
+            lock (this)
             {
-                _prng.GetBytes(buffer);
-                var index = BitConverter.ToUInt32(buffer, 0);
-                stringBuilder.Append(validCharacters[(int)(index % (uint)validCharactersLength)]);
-            }
+                var distinctValidCharacterArray = validCharacters.Distinct().ToArray();
 
-            return stringBuilder.ToString();
+                if (distinctValidCharacterArray.Length <= 1)
+                    throw new ArgumentException("Only one output is possible - output is determinable from inputs", nameof(validCharacters));
+
+                if (removeDuplicates)
+                    validCharacters = distinctValidCharacterArray;
+
+                var validCharactersLength = validCharacters.Length;
+
+                if (256 % validCharactersLength != 0)
+                    Trace.TraceWarning("The number of valid characters supplied cannot be evenly divided by 256. The entropy of the output is compromised.");
+
+                var stringBuilder = new StringBuilder();
+
+                RequestBuffer(length);
+
+                while (length-- > 0)
+                {
+                    _prng.GetBytes(_sharedBuffer);
+                    var index = BitConverter.ToUInt32(_sharedBuffer, _currentBufferIndex);
+                    stringBuilder.Append(validCharacters[(int)(index % (uint)validCharactersLength)]);
+                }
+
+                var result = stringBuilder.ToString();
+
+                _currentBufferIndex += length;
+
+                return result;
+            }
         }
 
         #endregion Strings
+
+        #region GC
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+                _handle.Dispose();
+
+            _disposed = true;
+        }
+
+        #endregion GC
     }
 }
